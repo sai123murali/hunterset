6356fec121f89c3b6346747be37155b4eb704f39
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2019-07-15 22:41:16 +0530

    Update subar.ly.py

--- subar.ly.py
+++ subar.ly.py
@@ -1,8 +1,8 @@
 a,b=map(int,input().split())
 li1=list(map(int,input().split()[:a]))
 li2=list(map(int,input().split()[:b]))
-flag=0
 for i in li2:
+    flag=0
     if(i in li1)and(li1.count(i)==li2.count(i)):
         flag=1
 if(flag==1):
